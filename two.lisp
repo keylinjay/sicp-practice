5ccc59b83f7c628eaeb1508c4ae36c2225639780
keylinjay <k547531366@hotmail.com>
2017-02-21 17:53:38 +0800

    update 2.11

--- two.lisp
+++ two.lisp
@@ -256,7 +256,7 @@
 
 ;;简单，答案略。
 
-;;;;3.0
+;;;;2.10
 
 (defun div-interval (x y)
   (if (< (* (lower-bound y)
@@ -266,4 +266,35 @@
       (mul-interval x (make-interval (/ 1.0 (upper-bound y))
 				     (/ 1.0 (lower-bound y))))))
 
-;;;;3.1
+;;;;2.11
+
+(defun mul-interval (x y)
+  (let ((x1 (lower-bound x))
+	(x2 (upper-bound x))
+	(y1 (lower-bound y))
+	(y2 (upper-bound y)))
+    
+    (cond ((and (>= x1 0) (>= x2 0)) (cond ((and (>= y1 0) (>= y2 0))
+					    (make-interval (* x1 y1) 
+							   (* y1 y2)))
+					   ((and (< y1 0) (< y2 0))
+					    (make-interval (* x2 y2) 
+							   (* x1 y1)))
+					   (t (make-interval (* x2 y1) 
+							     (* x2 y2)))))
+	  ((and (< x1 0) (< x2 0)) (cond ((and (>= y1 0) (>= y2 0))
+					  (make-interval (* x2 y2)
+							 (* x1 y1)))
+					 ((and (< y1 0) (< y2 0))
+					  (make-interval (* x1 y1)
+							 (* x2 y2)))
+					 (t (make-interval (* x2 y2)
+							   (* x2 y1)))))
+	  (t (cond ((and (>= y1 0) (>= y2 0))
+		    (make-interval (* x1 y2) (* x2 y2)))
+		   ((and (< y1 0) (< y2 0))
+		    (make-interval (* x2 y2) (* x1 y2)))
+		   (t (make-interval (* x1 y2) (* x2 y2))))))))
+	  
+					   
+					  
