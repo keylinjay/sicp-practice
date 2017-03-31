6e3b2d8c4fd04ea20fdcac11197ff2b0d3e52ae2
keylinjay <k547531366@hotmail.com>
2017-03-31 17:55:27 +0800

    update 3.22

--- three.lisp
+++ three.lisp
@@ -256,22 +256,148 @@
 ;;acc和acc2共享的部分只有make-account的外部环境。
 
 
-;;所谓的setf，只不过是一个宏。在car赋值的时候展开为(setq x (cons y (cdr x))),在赋值cdr的时候展开为(setq x (cons (car x) y)).不能将setf定义为函数的原因是，函数执行的时候将会创建新的环境和形参的约束。这样在函数内操作的只是该框架内的参数，并不能影响外部的需要修改的变量。
+;;所谓的setf，只不过是一个宏。它调用lisp内部的表操作函数rplaca和rplacd函数。
 
+(defun set-car! (c v)
+  (rplaca c v))
+
+(defun set-cdr! (c v)
+  (rplacd c v))
 
 
 (let ((x (cons 'a 'b))
       (y (cons 'c 'd)))
   (show x)
-  ;;set-cdr!的原理
-  (setq x (cons (car x) y))
+  (show (set-car! x y))
   (show x)
-  ;;set-car!的原理
-  (setq x (cons y (cdr x)))
-  (show x)
-  ;;在参数里执行setf并不会影响外面的x，这就是词法作用域。函数内的框架遮蔽了外面的。
-  (show (setf (car x) y))
-  (show x)
+  
   (show "end"))
+
+;;;;3.12
+;;(b)
+;;(b c d)
+
+;;;;3.13
+;;死循环
+
+;;;;3.14
+;;reverse
+;;v is (a), w is (c b a)
+
+;;;;3.16
+
+;;因为函数没有判断同一，所以同一个序对多引用一次就会多一次。环形结构就会死循环。
+
+;;;;3.17
+
+(defun count-pairs (lst)
+  (labels ((s-lst (lst res)
+	     (if (and (consp lst) (not (mem-eq lst res)))
+		 (s-lst (cdr lst) 
+			(s-lst (car lst) (cons lst res)))
+		 res))
+		    
+
+	   (mem-eq (item lst)
+	     (cond ((null lst) nil)
+		   ((eq item (car lst))
+		    (cdr lst))
+		   (t
+		    (mem-eq item (cdr lst))))))
+    (length (s-lst lst '()))))
+	   
+	 
+
+(let ((x (list 'a 'b))
+      (y (list 'c 'd)))
+  (show (count-pairs (cons x x)))
+  (show (count-pairs (list x y x y)))
+  (show "end"))
+
+;;;;3.18 3.19
+;;只需要判断lst的cdr部分是否和原lst是同一就行了。
+
+(defun cyclep (lst)
+  (labels ((test (l1)
+	     (cond ((null (cdr l1)) nil)
+		   ((eq (cdr l1) lst)
+		    t)
+		   (t
+		    (test (cdr l1))))))
+    (test lst)))
+
+(defun last-pair (lst)
+  (if (null (cdr lst))
+      lst
+      (last-pair (cdr lst))))
+
+(defun append! (lst1 lst2)
+  (set-cdr! (last-pair lst1) lst2)
+  lst1)
+
+(defun make-cycle (x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(let ((x (list 'a 'b 'c)))
+  (show (cyclep x))
+  (show (cyclep (make-cycle x)))
+  (show "3.19 end"))
+
+
+;;;;3.22
+
+(defun make-queue ()
+  (let ((front-ptr '())
+	(rear-ptr '()))
+    (labels ((set-front-ptr! (item)
+	       (setq front-ptr item))
+	     (set-rear-ptr! (item)
+	       (setq rear-ptr item))
+	     
+	     (dispatch (m)
+	       (cond ((eq m 'front-ptr) front-ptr)
+		     ((eq m 'rear-ptr) rear-ptr)
+		     ((eq m 'set-front-ptr!) #'set-front-ptr!)
+		     ((eq m 'set-rear-ptr!) #'set-rear-ptr!)
+		     (t
+		      (error "not this inner method -- make-queue~A" m)))))
+      #'dispatch)))
+
+(defun front-ptr (queue) (funcall queue 'front-ptr))
+(defun rear-ptr (queue) (funcall queue 'rear-ptr))
+(defun set-front-ptr! (queue item)
+  (funcall (funcall queue 'set-front-ptr!)
+	   item))
+(defun set-rear-ptr! (queue item)
+  (funcall (funcall queue 'set-rear-ptr!)
+	   item))
+
+(defun empty-queue? (queue)
+  (null (front-ptr queue)))
+
+(defun front-queue (queue)
+  (if (empty-queue? queue)
+      (error "front called with an empty queue ~A" queue)
+      (car (front-ptr queue))))
+
+(defun insert-queue! (queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+	   (set-front-ptr! queue new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue)
+	  (t
+	   (set-cdr! (rear-ptr queue) new-pair)
+	   (set-rear-ptr! queue new-pair)
+	   queue))))
+(defun delete-queue! (queue)
+  (cond ((empty-queue? queue)
+	 (error "delete! called with an empty queue~A" queue))
+	(t
+	 (set-front-ptr! queue (cdr (front-ptr queue)))
+	 queue)))
+	   
+;;;;3.23
 
 
